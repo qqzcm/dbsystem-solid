4ec66a3074c0268692337c0b232a234cd0265b7a
Lok <852734717@qq.com>
2024-03-08 21:50:27 +0800

    修改欧氏距离计算函数，采用米为单位，并在此基础上缩小；修改bstd.js,进行数据显示测试

--- app/src/main/resources/static/js/bstd.js
+++ app/src/main/resources/static/js/bstd.js
@@ -1,69 +1,36 @@
 import utils from "./utils.js";
 
-async function LoadBSTD(vueThis, longitude, latitude, keywords) {
+async function LoadBSTD(vueThis) {
+    vueThis.spatial_skylines.loading = true;
+    vueThis.spatial_skylines.timeout = false;
 
-    let markers = await loadObjectPoints(vueThis);
+    let longitude = vueThis.spatial_skylines.query.longitude;
+    let latitude = vueThis.spatial_skylines.query.latitude;
 
-    vueThis.map = new mapboxgl.Map({
-        container: 'map',
-        style: vueThis.mapStyle,
-        center: [longitude, latitude],
-        zoom: 10
-    });
+    let objectData = await loadData(vueThis);
 
-    vueThis.map.doubleClickZoom.disable();
-    console.log("current position: " + longitude + "," + latitude);
-    let marker = utils.currentPosition(longitude, latitude);
-    marker.setPopup(utils.getPopUp("当前位置", false));
-    marker.addTo(vueThis.map);
-    vueThis.spatial_skylines.curMarker = marker;
+    vueThis.spatial_skylines.lastKeywords = vueThis.spatial_skylines.query.keywords.split(",");
 
-    vueThis.map.on('dblclick', (e) => {
-        console.log(`A click event has occurred at ${e.lngLat}`);
-        if (vueThis.spatial_skylines.curMarker != null) {
-            vueThis.spatial_skylines.curMarker.remove();
-        }
-        vueThis.spatial_skylines.query.longitude = e.lngLat.lng;
-        vueThis.spatial_skylines.query.latitude = e.lngLat.lat;
+    paintMap(vueThis, longitude, latitude);
 
-        let marker = utils.currentPosition(e.lngLat.lng, e.lngLat.lat);
-        marker.setPopup(utils.getPopUp("当前位置", false));
-        vueThis.spatial_skylines.curMarker = marker;
-        marker.addTo(vueThis.map);
-    });
+    paintCurrentLocation(vueThis, longitude, latitude, objectData);
 
-    await paintPoints(vueThis, markers.length);
+    await paintPoints(vueThis, objectData.data.length);
 
-    await paintMarker(vueThis, markers);
-
+    vueThis.spatial_skylines.loading = false;
 }
 
-async function loadObjectPoints(vueThis) {
-    let url = vueThis.baseUrl + '/bstd/objectPoints?'
+// 异步加载后端运行的Skyline结果集
+async function loadData(vueThis) {
+    let path = vueThis.baseUrl + '/bstd/objectPoints?'
         + 'longitude=' + vueThis.spatial_skylines.query.longitude
         + '&latitude=' + vueThis.spatial_skylines.query.latitude
         + '&keywords=' + vueThis.spatial_skylines.query.keywords;
-
-    let objectPoints = [];
-    let res = requestObjectPoints(url);
-    for (let i = 0; i < res.data.length; i++) {
-        let objPoint = res.data[i];
-        let color = utils.getColor(i, res.data.length);
-        let marker = utils.getDefaultMark(objPoint.longitude, objPoint.latitude, color);
-
-        /**
-         * Todo
-         */
-        marker.setPopup(
-            utils.getPopUp(
-                "<strong>skylineId " + objPoint.objId + "</strong>"
-            )
-        );
-        objectPoints.push(marker);
-    }
-    return objectPoints;
+    let res = await requestObjectPoints(path);
+    return res;
 }
 
+// get方法请求
 async function requestObjectPoints(url) {
     return axios({
         method: 'get',
@@ -71,6 +38,29 @@ async function requestObjectPoints(url) {
     });
 }
 
+// 绘制地图
+function paintMap(vueThis, longitude, latitude) {
+    vueThis.map = new mapboxgl.Map({
+        container: 'map', // container id
+        style: vueThis.mapStyle,
+        center: [longitude, latitude],
+        zoom: 10 // 放大级别
+    });
+}
+
+// 绘制当前位置标记
+function paintCurrentLocation(vueThis, longitude, latitude, objectData) {
+    let currentLocationMarker = new mapboxgl.Marker({
+        color: '#ff0505',
+        scale: 1.4
+    }).setLngLat([longitude, latitude]);
+    currentLocationMarker.setPopup(utils.getPopUp(
+        "<strong>当前位置</strong>" +
+        "<p>pointNum: " + objectData.data.length + "</p>", false));
+    currentLocationMarker.addTo(vueThis.map);
+}
+
+// 绘制Skyline结果点
 async function paintPoints(vueThis, size) {
     let url = vueThis.baseUrl + '/bstd/geojson?'
         + 'longitude=' + vueThis.spatial_skylines.query.longitude
@@ -95,16 +85,17 @@ async function paintPoints(vueThis, size) {
                     'circle-opacity': 0.7,
                 },
             });
-            //layerPopup(i, vueThis);
+            layerPopup(i, vueThis);
         }
-    })
+    });
 }
 
+// 添加功能：点击Skyline点可以展示详细信息
 function layerPopup(i, vueThis) {
     vueThis.map.on('click', 'layer' + i, function (e) {
         let coordinates = e.features[0].geometry.coordinates.slice();
         let labels = JSON.parse(e.features[0].properties.labels);
-        var strings = vueThis.spatial_skylines.lastKeywords;
+        let strings = vueThis.spatial_skylines.lastKeywords;
 
         let str = "";
         for (let j = 0; j < labels.length; j++) {
@@ -140,78 +131,6 @@ function layerPopup(i, vueThis) {
     });
 }
 
-async function paintMarker(vueThis, markers) {
-    for (let i = 0; i < markers.length; i++) {
-        markers[i].addTo(vueThis.map);
-    }
-}
-
-async function LoadBSTD2(vueThis, longitude, latitude) {
-    vueThis.KSTC.loading=true;
-    vueThis.KSTC.timeout=false;
-
-    let path = vueThis.baseUrl + '/bstd/objectPoints?'
-        + 'longitude=' + vueThis.spatial_skylines.query.longitude
-        + '&latitude=' + vueThis.spatial_skylines.query.latitude
-        + '&keywords=' + vueThis.spatial_skylines.query.keywords;
-    let markers = [];
-    let res = await requestObjectPoints(path);
-    for (let i = 0; i < res.data.length; i++) {
-        let mrk = res.data[i];
-        let color = utils.getColor(i, res.data.length);
-        let marker = utils.getDefaultMark(mrk.coordinate.longitude, mrk.coordinate.latitude, color);
-        marker.setPopup(
-            utils.getPopUp(
-                "<strong>objectId " + mrk.objId + "</strong>"
-            )
-        );
-        markers.push(marker);
-    }
-    vueThis.spatial_skylines.lastKeywords = vueThis.spatial_skylines.query.keywords.split(",");
-
-    vueThis.map = new mapboxgl.Map({
-        container: 'map', // container id
-        style: vueThis.mapStyle,
-        center: [longitude, latitude],
-        zoom: 10
-    });
-
-    let currentLocationMarker = new mapboxgl.Marker({
-        color: '#ff0505',
-        scale: 1
-    }).setLngLat([longitude, latitude]);
-    currentLocationMarker.setPopup(utils.getPopUp("当前位置", false));
-    currentLocationMarker.addTo(vueThis.map);
-
-    await paintPoints(vueThis, markers.length);
-
-    await paintMarker(vueThis, markers);
-
-    vueThis.spatial_skylines.loading=false;
-
-    // await axios({
-    //     method: "get",
-    //     url: path
-    // });
-    //
-    // vueThis.map.on('load', () => {
-    //     vueThis.map.addSource('points-source', {
-    //         type: 'geojson',
-    //         url: url
-    //     });
-    // });
-    //
-    // vueThis.map.addLayer({
-    //     'type': 'circle',
-    //     'paint': {
-    //         'circle-radius': 3,
-    //         'circle-color': '#e55e5e',
-    //         'circle-opacity': 0.7
-    //     }
-    // });
-}
-
 export default {
-    //LoadBSTD,
-    LoadBSTD2
+    LoadBSTD
 }
